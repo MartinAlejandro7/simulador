78f7088c256974913e50f1fd4e77bf924a45daf3
MartinAlejandro7 <alejomartin7532@gmail.com>
2026-07-01 21:33:22 -0500

    calcular saldo disponible según ingresos y egresos

--- funciones.js
+++ funciones.js
@@ -1,1 +1,11 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+function calcularDisponibilidad(ingresos, egresos){
+    let valorInicial = 0
+    let calcularValor = ingresos - egresos
+    if (calcularValor < valorInicial ){
+        return valorInicial
+    }
+    return calcularValor
+}
+
--- simulador.js
+++ simulador.js
@@ -1,1 +1,20 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular(){
+    //Recuperar INGRESOS y Pasar a float
+    let valorIngresos= document.getElementById("txtIngresos")
+    let valorIngresosStr = valorIngresos.value;
+    let valorIngresosFloat = parseFloat(valorIngresosStr);
+
+    //Recuperar EGRESOS y Pasar a float
+    let valorEgresos = document.getElementById("txtEgresos")
+    let valorEgresosStr = valorEgresos.value;
+    let valorEgresosFloat = parseFloat(valorEgresosStr);
+    
+    //Llamar a la funcion CalcularDisponiblidad 
+
+    let total = calcularDisponibilidad(valorIngresosFloat, valorEgresosFloat);
+    let totalValor = total.toFixed(2)
+    let contenedorDisponible =document.getElementById("spnDisponible")
+    contenedorDisponible.textContent = totalValor
+
+}
